044a544bfafc6e6b7c211f3dc35e3e5250d3e650
Vinoth30457 <vinoth30457@gmail.com>
2023-07-05 16:25:32 +0530

    clean template file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-"use strict";
+import React from "react";
+import ReactDom from "react-dom/client";
+
+import App from "./App";
+
+import "./index.css";
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+// const rootEl = document.getElementById("root");
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
